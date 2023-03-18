8ebb2bbe1624a29a41ab8241be2431f5b0978137
RAKESH BODAVULA <rakeshbodavula2002@gmail.com>
2023-03-18 09:18:54 +0530

    added morgan and helmet middlewares

--- server/server.js
+++ server/server.js
@@ -4,15 +4,28 @@ const bodyParser = require('body-parser')
 const authRoutes = require('./routes/authRoutes')
 const cors = require('cors')
 const dotenv = require('dotenv')
+const helmet = require("helmet")
+const morgan = require("morgan")
+const path = require("path")
+const rfs = require("rotating-file-stream")
 
 const app = express()
 
 
+let accessLogStream = rfs.createStream('access.log',{
+        interval : '1d', // 1 day
+        path:path.join(__dirname,"Logs")
+})
+
+
+
 // middleware
+app.use(morgan('combined',{stream:accessLogStream}))
 app.use(express.urlencoded({extended : true})) // Built-in middleware
 app.use(express.json()) // Built-in middleware
 app.use(bodyParser.json()) // Third Party middleware
 app.use(cors())
+app.use(helmet())
 
 
 // database connectivity
